9e2d10f1bc8228b8f6cf04ddeb223f62844a40c4
/C=US/ST=Massachusetts/L=Cambridge/O=Wilson Lab, PCLM, MIT/OU=The Soma Project/CN=jonas/emailAddress=jonas@soma.mit.edu </C=US/ST=Massachusetts/L=Cambridge/O=Wilson Lab, PCLM, MIT/OU=The Soma Project/CN=jonas/emailAddress=jonas@soma.mit.edu@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2004-11-14 03:02:46 +0000

    Totally rewrote this test suite to be entirely behavioral
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/hardware/network@764 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

--- simulations/GMIIin/GMIIintest.vhd
+++ simulations/GMIIin/GMIIintest.vhd
@@ -47,6 +47,10 @@ ARCHITECTURE behavior OF GMIIintest IS
 	SIGNAL VALID :  std_logic;
 	SIGNAL DOUT :  std_logic_vector(7 downto 0);
 
+	-- synchronization signals 
+	signal instate, outstate : integer := 0;
+
+
 BEGIN
 
 	uut: gmiiin PORT MAP(
@@ -68,77 +72,132 @@ BEGIN
 
 	
 	-- input data:
-	inputdata: process is
-		file gmiifile: text; 
-		variable L : line; 
-		variable dvin : integer := 0;
-		variable erin : integer := 0; 
-		variable rxdin : std_logic_vector(7 downto 0);
 
-	begin					 	
-		file_open(gmiifile, "gmii.dat", read_mode); 
-		while not endfile(gmiifile) loop
-			wait until rising_edge(RX_CLK); 
-			readline(gmiifile, L);
-			hread(L, rxdin);
-			read(L, dvin);
-			read(L, erin); 
+	dataout: process is
 
-			if dvin = 0 then
-				RX_DV <= '0';
-			elsif dvin = 1 then
-				RX_DV <= '1';
-			end if; 
 
-			if erin = 0 then
-				RX_ER <= '0';
-			else 
-				RX_ER <= '1';
-			end if; 
-			RXD <= rxdin; 
-		end loop; 
-		wait; 
-
-		--assert false
-			--report "End of simulation"
-			--severity failure;   
-	end process; 
-
-	-- output check data:
-	output: process is
-		file outfile: text; 
-		variable L : line; 
-		variable din : std_logic_vector(15 downto 0);
-
-	begin					 	
-		file_open(outfile, "output.dat", read_mode); 
-		while not endfile(outfile) loop
-			NEXTF <= '1';	
-			wait until rising_edge(CLK); 
-			NEXTF <= '0';
-			readline(outfile, L);
-			for i in 0 to 100000 loop
+		procedure readFIFO(constant len : in integer;
+					    constant startdata : in std_logic_vector(7 downto 0);
+					    constant errorf : in integer := 0	) is	
+			variable data : std_logic_vector(7 downto 0); 
+			variable done : integer := 0;
+			variable bcnt : integer := 0;  
+		begin
+			data := startdata;
+			NEXTF <= '1'; 
+			bcnt := 0; 
+			done := 0; 
+			wait until rising_edge(CLK);
+			while done = 0 loop
+				NEXTF <= '0'; 	 
 				wait until rising_edge(CLK);
-				if VALID = '1' then		
-					hread(L, din);
-					if DIN(15) = '1' and ENDFOUT /= '1' then
-						report "Error in detecting correct end of frame";
-					else 
-						if din(7 downto 0) /= DOUT
-							and ENDFOUT = '0'  then
-							report "error in DIN"; 
-						end if;
-					end if;   
-					if ENDFOUT = '1' then
-						exit;
+				if VALID = '1' then
+				   if ENDFOUT = '0' then
+				   	-- normal data read
+					bcnt := bcnt + 1; 
+					if DOUT(7 downto 0) /= data then
+						report "Data read error";
+					end if; 
+
+ 				   else
+				     if EROUT = '1' or OFOUT = '1' then
+					   if errorf = 0 then
+					   	report "Frame was an unexpected error";
+					   end if;  
 					end if;  
+					done := 1; 
+				   end if; 
+
+			    	   data := (data(6 downto 0) & data(7)); 
 				end if; 
 			end loop; 
-		end loop; 
+			
+
+		end procedure readFIFO; 
+
+
+	begin
+		wait for 100 ns; 		
+		wait until instate = 1;
+		wait for 100 ns; 
+		readFIFO(100, X"01"); 
+		wait until instate = 3; 
+		wait for 10 ns; 
+		readFIFO(200, X"F1");
+		readFIFO(300, X"4C"); 
+		
+		readFIFO(50, X"05", 1); 
+		outstate <= 1;
+		wait until instate = 4; 
+		readFIFO(1500, X"70", 1);
+		outstate <= 2; 
+		wait for 400 ns;  
+		readFIFO(500, X"48"); 	
+		outstate <= 3;
+		wait until instate = 5; 
+		readFIFO(400, X"A7");
+		outstate <= 4;  	
+   		readFIFO(800, X"75", 1); -- this one should fail 
+		readFIFO(400, X"7B");
+
 		wait; 
+	end process; 
+
+	datain: process is
+		procedure writeFIFO(constant len : in integer;
+					constant startdata : in 
+						std_logic_vector(7 downto 0);
+				     constant errloc : in integer := -1 ) is
+			variable data : std_logic_vector(7 downto 0); 
+		begin 
+			data := startdata; 
+			for i in 1 to len loop
+				RX_DV <= '1';
+				RXD <= data; 
+				if i = errloc then
+					RX_ER <= '1'; 
+				else	
+					RX_ER <= '0'; 
+				end if; 			
+				wait until rising_edge(RX_CLK); 
+				data := (data(6 downto 0) & data(7)); 
+			end loop; 
+			RX_DV <= '0'; 
+			wait until rising_edge(RX_CLK); 
+
+		end procedure writeFIFO; 
+	begin
+		wait for 100 ns; 
+		instate <= 1; 
+
+		writeFIFO(100, X"01"); 		
+		writeFIFO(200, X"F1"); 
+		instate <= 3; 	
+		writeFIFO(300, X"4C"); 
+		writeFIFO(50, X"05", 10); 
+		wait until outstate = 1; 
+
+		-- now, we're going to try and break the fifo
+		writeFIFO(1500, X"70"); -- huge fifo
+		instate <= 4;
+		wait until outstate = 2; 
+		-- can we write to the fifo again without problems?
+		writeFIFO(500, X"48"); 
+		wait until outstate = 3; 
+		-- now, we write a good frame followed by an overflow
+		writeFIFO(400, X"A7"); 
+		writeFIFO(800, X"75"); 
+		instate <= 5;
+		wait until outstate = 4; 
+		writeFIFO(400, X"7B");  
+
+
 		--assert false
 			--report "End of simulation"
-			--severity failure;   
-	end process; 
+			--severity failure;
+
+		wait; 
+	end process datain; 
+
 
 END;
