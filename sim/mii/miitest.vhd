12c8fc83b02188623cce8db0f7e5c78c9d8e9ac8
/C=US/ST=Massachusetts/L=Cambridge/O=soma/CN=Eric Jonas/emailAddress=jonas@soma.mit.edu </C=US/ST=Massachusetts/L=Cambridge/O=soma/CN=Eric Jonas/emailAddress=jonas@soma.mit.edu@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2006-06-20 16:11:29 +0000

    * Completed the MIItest and upgraded to modern sim framework
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/network/trunk@1539 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

--- sim/mii/miitest.vhd
+++ sim/mii/miitest.vhd
@@ -1,161 +1,153 @@
 
--- VHDL Test Bench Created from source file mii.vhd -- 23:37:25 10/16/2004
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends 
--- that these types always be used for the top-level I/O of a design in order 
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
-USE ieee.numeric_std.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
-ENTITY miitest IS
-END miitest;
+entity miitest is
+end miitest;
 
-ARCHITECTURE behavior OF miitest IS 
+architecture behavior of miitest is
 
-	COMPONENT mii
-	PORT(
-		CLK : IN std_logic;
-		CLKSLEN : IN std_logic;
-		RESET : IN std_logic;
-		DIN : IN std_logic_vector(15 downto 0);
-		ADDR : IN std_logic_vector(4 downto 0);
-		START : IN std_logic;
-		RW : IN std_logic;    
-		MDIO : INOUT std_logic;      
-		MDC : OUT std_logic;
-		DOUT : OUT std_logic_vector(15 downto 0);
-		DONE : OUT std_logic
-		);
-	END COMPONENT;
+  component mii
+    port(
+      CLK     : in    std_logic;
+      CLKSLEN : in    std_logic;
+      RESET   : in    std_logic;
+      DIN     : in    std_logic_vector(15 downto 0);
+      ADDR    : in    std_logic_vector(4 downto 0);
+      START   : in    std_logic;
+      RW      : in    std_logic;
+      MDIO    : inout std_logic;
+      MDC     : out   std_logic;
+      DOUT    : out   std_logic_vector(15 downto 0);
+      DONE    : out   std_logic
+      );
+  end component;
 
-	component GMII is
-	    Port ( MDIO : inout std_logic;
-	           MDC : in std_logic;
-	           DONE : out std_logic);
-	end component;
+  component GMII is
+                   port ( MDIO : inout std_logic;
+                          MDC  : in    std_logic;
+                          DONE : out   std_logic);
+  end component;
 
-	SIGNAL CLK :  std_logic := '0';
-	SIGNAL CLKSLEN :  std_logic := '0';
-	SIGNAL RESET :  std_logic := '1';
-	SIGNAL MDIO :  std_logic;
-	SIGNAL MDC :  std_logic;
-	SIGNAL DIN :  std_logic_vector(15 downto 0);
-	SIGNAL DOUT :  std_logic_vector(15 downto 0);
-	SIGNAL ADDR :  std_logic_vector(4 downto 0);
-	SIGNAL START :  std_logic;
-	SIGNAL RW :  std_logic;
-	SIGNAL DONE :  std_logic;
+  signal CLK     : std_logic := '0';
+  signal CLKSLEN : std_logic := '0';
+  signal RESET   : std_logic := '1';
+  signal MDIO    : std_logic;
+  signal MDC     : std_logic;
+  signal DIN     : std_logic_vector(15 downto 0);
+  signal DOUT    : std_logic_vector(15 downto 0);
+  signal ADDR    : std_logic_vector(4 downto 0);
+  signal START   : std_logic;
+  signal RW      : std_logic;
+  signal DONE    : std_logic;
 
-BEGIN
+begin
 
-	uut: mii PORT MAP(
-		CLK => CLK,
-		CLKSLEN => CLKSLEN,
-		RESET => RESET,
-		MDIO => MDIO,
-		MDC => MDC,
-		DIN => DIN,
-		DOUT => DOUT,
-		ADDR => ADDR,
-		START => START,
-		RW => RW,
-		DONE => DONE
-	);
+  uut : mii port map(
+    CLK     => CLK,
+    CLKSLEN => CLKSLEN,
+    RESET   => RESET,
+    MDIO    => MDIO,
+    MDC     => MDC,
+    DIN     => DIN,
+    DOUT    => DOUT,
+    ADDR    => ADDR,
+    START   => START,
+    RW      => RW,
+    DONE    => DONE
+    );
 
-	gmii_uut : gmii port map (
-		MDIO => MDIO,
-		MDC => MDC, 
-		DONE => open); 
+  gmii_uut : gmii port map (
+    MDIO => MDIO,
+    MDC  => MDC,
+    DONE => open);
 
-	clk <= not clk after 4 ns;
-	RESET <= '0' after 20 ns; 
-	clock_enable: process(CLK) is
-		variable clkcount : integer range 0 to 7 := 0;
+  clk   <= not clk after 4 ns;
+  RESET <= '0'     after 20 ns;
 
-	begin
-		if rising_edge(CLK) then
-		   if clkcount = 7 then	
-		   	CLKSLEN <= '1';
-			clkcount := 0;
-		   else
-		   	CLKSLEN <= '0';
-			clkcount := clkcount + 1;
-		   end if; 
-		end if;		
-	end process; 
+  clock_enable        : process(CLK)
+    variable clkcount : integer range 0 to 7 := 0;
+
+  begin
+    if rising_edge(CLK) then
+      if clkcount = 7 then
+        CLKSLEN <= '1';
+        clkcount := 0;
+      else
+        CLKSLEN <= '0';
+        clkcount := clkcount + 1;
+      end if;
+    end if;
+  end process;
 
 
-	main : process is
-	begin
-		DIN <= X"0000"; 
-		ADDR <= "00000"; 
-		START <= '0';
-		RW <= '0'; 
+  main : process is
+  begin
+    DIN   <= X"0000";
+    ADDR  <= "00000";
+    START <= '0';
+    RW    <= '0';
 
-		wait for 10 us; 
-		DIN <= X"ABCD";
-		ADDR <= "00000";
-		RW <= '1'; 
-		wait until rising_edge(CLK) and CLKSLEN = '1';
-		START <= '1';
-		wait until rising_edge(CLK) and CLKSLEN = '1';
-    		START <= '0';
-		wait until rising_edge(CLK) 
-			and CLKSLEN = '1'
-			and DONE = '1'; 
+    wait for 10 us;
+    DIN   <= X"ABCD";
+    ADDR  <= "00000";
+    RW    <= '1';
+    wait until rising_edge(CLK) and CLKSLEN = '1';
+    START <= '1';
+    wait until rising_edge(CLK) and CLKSLEN = '1';
+    START <= '0';
+    wait until rising_edge(CLK)
+      and CLKSLEN = '1'
+      and DONE = '1';
 
-		wait for 10 us; 
-		DIN <= X"1234";
-		ADDR <= "00100";
-		RW <= '1'; 
-		wait until rising_edge(CLK) and CLKSLEN = '1';
-		START <= '1';
-		wait until rising_edge(CLK) and CLKSLEN = '1';
-    		START <= '0';
-		wait until rising_edge(CLK) 
-			and CLKSLEN = '1'
-			and DONE = '1'; 
+    wait for 10 us;
+    DIN   <= X"1234";
+    ADDR  <= "00100";
+    RW    <= '1';
+    wait until rising_edge(CLK) and CLKSLEN = '1';
+    START <= '1';
+    wait until rising_edge(CLK) and CLKSLEN = '1';
+    START <= '0';
+    wait until rising_edge(CLK)
+      and CLKSLEN = '1'
+      and DONE = '1';
 
-		wait for 10 us; 
-		DIN <= X"1234";
-		ADDR <= "00000";
-		RW <= '0'; 
-		wait until rising_edge(CLK) and CLKSLEN = '1';
-		START <= '1';
-		wait until rising_edge(CLK) and CLKSLEN = '1';
-    		START <= '0';
-		wait until rising_edge(CLK) 
-			and CLKSLEN = '1'
-			and DONE = '1'; 
-		assert DOUT = X"ABCD" 
-			report "Error reading data at address 0"
-			severity error; 
+    wait for 10 us;
+    DIN   <= X"1234";
+    ADDR  <= "00000";
+    RW    <= '0';
+    wait until rising_edge(CLK) and CLKSLEN = '1';
+    START <= '1';
+    wait until rising_edge(CLK) and CLKSLEN = '1';
+    START <= '0';
+    wait until rising_edge(CLK)
+      and CLKSLEN = '1'
+      and DONE = '1';
+    assert DOUT = X"ABCD"
+      report "Error reading data at address 0"
+      severity error;
 
-		wait for 10 us; 
-		DIN <= X"7777";
-		ADDR <= "00100";
-		RW <= '0'; 
-		wait until rising_edge(CLK) and CLKSLEN = '1';
-		START <= '1';
-		wait until rising_edge(CLK) and CLKSLEN = '1';
-    		START <= '0';
-		wait until rising_edge(CLK) 
-			and CLKSLEN = '1'
-			and DONE = '1'; 
-		assert DOUT = X"1234" 
-			report "Error reading data at address 0"
-			severity error; 
-		
-		assert false
-			report "End of Simulation"
-			severity failure; 
-		 
+    wait for 10 us;
+    DIN   <= X"7777";
+    ADDR  <= "00100";
+    RW    <= '0';
+    wait until rising_edge(CLK) and CLKSLEN = '1';
+    START <= '1';
+    wait until rising_edge(CLK) and CLKSLEN = '1';
+    START <= '0';
+    wait until rising_edge(CLK)
+      and CLKSLEN = '1'
+      and DONE = '1';
+    assert DOUT = X"1234"
+      report "Error reading data at address 100"
+      severity error;
 
-	end process main; 
+    assert false
+      report "End of Simulation"
+      severity failure;
 
-END;
+
+  end process main;
+
+end;
