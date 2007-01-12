b04ee78260e6479af0110c03cde3b342d834a441
/C=US/ST=Massachusetts/L=Cambridge/O=soma/CN=Eric Jonas/emailAddress=jonas@soma.mit.edu </C=US/ST=Massachusetts/L=Cambridge/O=soma/CN=Eric Jonas/emailAddress=jonas@soma.mit.edu@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2007-01-12 23:27:42 +0000

    * updates to memtest to work with FMF model
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/network/trunk@1997 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

--- tests/memtest/sim/memtesttest.vhd
+++ tests/memtest/sim/memtesttest.vhd
@@ -33,13 +33,6 @@ architecture Behavioral of memtesttest is
   end component;
 
   component NoBLSRAM
-    generic ( FILEIN       :       string  := "SRAM_in.dat";
-              FILEOUT      :       string  := "SRAM_out.dat";
-              physical_sim :       integer := 0;
-              TSU          :       time;
-              THD          :       time;
-              TKQ          :       time;
-              TKQX         :       time);
     port ( CLK             : in    std_logic;
            DQ              : inout std_logic_vector(31 downto 0);
            ADDR            : in    std_logic_vector(16 downto 0);
@@ -53,7 +46,7 @@ architecture Behavioral of memtesttest is
   signal MD    : std_logic_vector(31 downto 0) := (others => '0');
   signal MWE   : std_logic                     := '0';
   signal MA    : std_logic_vector(16 downto 0) := (others => '0');
-  signal MCLKout, MCLKin  : std_logic                     := '0';
+  signal MCLK  : std_logic                     := '0';
   signal MOE   : std_logic                     := '0';
 
   signal PHYRESET : std_logic := '0';
@@ -62,47 +55,6 @@ architecture Behavioral of memtesttest is
   signal LED1000  : std_logic := '0';
   signal LEDACT   : std_logic := '0';
 
-  component mt55l256l32p
-    generic (
-      -- Constant parameters
-      addr_bits : integer := 17;
-      data_bits : integer := 32;
-
-      -- Timing parameters for -10 (100 Mhz)
-      tKHKH : time := 10.0 ns;
-      tKHKL : time := 3.2 ns;
-      tKLKH : time := 3.2 ns;
-      tKHQV : time := 5.0 ns;
-      tAVKH : time := 2.0 ns;
-      tEVKH : time := 2.0 ns;
-      tCVKH : time := 2.0 ns;
-      tDVKH : time := 2.0 ns;
-      tKHAX : time := 0.5 ns;
-      tKHEX : time := 0.5 ns;
-      tKHCX : time := 0.5 ns;
-      tKHDX : time := 0.5 ns
-      );
-
-    -- Port Declarations
-    port (
-      Dq    : inout std_logic_vector (data_bits - 1 downto 0);  -- Data I/O
-      Addr  : in    std_logic_vector (addr_bits - 1 downto 0);  -- Address
-      Lbo_n : in    std_logic;                                  -- Burst Mode
-      Clk   : in    std_logic;                                  -- Clk
-      Cke_n : in    std_logic;                                  -- Cke#
-      Ld_n  : in    std_logic;                                  -- Adv/Ld#
-      Bwa_n : in    std_logic;                                  -- Bwa#
-      Bwb_n : in    std_logic;                                  -- BWb#
-      Bwc_n : in    std_logic;                                  -- Bwa#
-      Bwd_n : in    std_logic;                                  -- BWb#
-      Rw_n  : in    std_logic;                                  -- RW#
-      Oe_n  : in    std_logic;                                  -- OE#
-      Ce_n  : in    std_logic;                                  -- CE#
-      Ce2_n : in    std_logic;                                  -- CE2#
-      Ce2   : in    std_logic;                                  -- CE2
-      Zz    : in    std_logic                                   -- Snooze Mode
-      );
-  end component;
 
 begin  -- Behavioral
 
@@ -114,37 +66,24 @@ begin  -- Behavioral
       MWE      => MWE,
       MA       => MA,
       MOE      => MOE,
-      MCLK     => MCLKout,
+      MCLK     => MCLK,
       PHYRESET => PHYRESET,
       LEDPOWER => LEDPOWER,
       LED100   => LED100,
       LED1000  => LED1000,
       LEDACT   => LEDACT);
 
-  memory : mt55l256l32p
+  memory : noblsram
     port map (
-      Dq    => MD,
-      addr  => MA,
-      Lbo_n => '0',
-      CLK   => MCLKin,
-      CKE_N => '0',
-      LD_N  => '0',
-      BWA_N => '0',
-      BWB_N => '0',
-      BWC_N => '0',
-      BWD_N => '0',
-      RW_N  => MWE,
-      OE_N  => MOE,
-      CE_N  => '0',
-      CE2_N => '0',
-      CE2   => '1',
-      Zz    => '0');
+      CLK => MCLK,
+      DQ => MD,
+      ADDR => MA,
+      WE => MWE,
+      RESET => RESET,
+      SAVE => '0');
 
   CLKIN <= not CLKIN after 10 ns;
-  RESET <= '0'       after 20 ns;
-
-
-  MCLKin <= not MCLKout after 9.5 ns;
+  RESET <= '0'       after 200 ns;
   
 
 end Behavioral;
--- tests/memtest/vhdl/memtest.vhd
+++ tests/memtest/vhdl/memtest.vhd
@@ -10,7 +10,6 @@ use UNISIM.VComponents.all;
 
 
 entity memtest is
-
   port (
     CLKIN    : in    std_logic;
     RESET    : in    std_logic;
@@ -25,33 +24,42 @@ entity memtest is
     LED1000  : out   std_logic;
     LEDACT   : out   std_logic
     );
-
 end memtest;
 
 architecture Behavioral of memtest is
 
-  signal clk : std_logic := '0';
+  -- clock and timing signals
+  signal clk, clkio, clklo, clkrx             : std_logic := '0';
+  signal clkint, clkioint, clkloint, clkrxint : std_logic := '0';
+  signal clkb, clkbint                        : std_logic := '0';
+  signal clkf, clkfint                        : std_logic := '0';
 
-  signal addr1  : std_logic_vector(16 downto 0) := '0' & X"0005";
+  signal clk90, clk90int   : std_logic := '0';
+  signal clk180, clk180int : std_logic := '0';
+  signal clk270, clk270int : std_logic := '0';
+
+
+
+  signal addr1  : std_logic_vector(16 downto 0) := '0' & X"0000";
   signal d1     : std_logic_vector(31 downto 0) := (others => '0');
   signal q1     : std_logic_vector(31 downto 0) := (others => '0');
   signal we1    : std_logic;
   signal clken1 : std_logic                     := '0';
 
-  signal addr2    : std_logic_vector(16 downto 0) := (others => '0'); 
+  signal addr2                          : std_logic_vector(16 downto 0) := '0' & X"FFF0";
   signal expected, expected2, expected3 : std_logic_vector(31 downto 0) := (others => '0');
-  signal d2       : std_logic_vector(31 downto 0) := (others => '0');
-  signal q2       : std_logic_vector(31 downto 0) := (others => '0');
-  signal we2      : std_logic;
-  signal clken2   : std_logic                     := '0';
+  signal d2                             : std_logic_vector(31 downto 0) := (others => '0');
+  signal q2                             : std_logic_vector(31 downto 0) := (others => '0');
+  signal we2                            : std_logic;
+  signal clken2                         : std_logic                     := '0';
 
-  signal addr3  : std_logic_vector(16 downto 0) := (others => '0');
+  signal addr3  : std_logic_vector(16 downto 0) := '1' & X"0000";
   signal d3     : std_logic_vector(31 downto 0) := (others => '0');
   signal q3     : std_logic_vector(31 downto 0) := (others => '0');
   signal we3    : std_logic;
   signal clken3 : std_logic                     := '0';
 
-  signal addr4  : std_logic_vector(16 downto 0) := (others => '0');
+  signal addr4  : std_logic_vector(16 downto 0) := '1' & X"0000";
   signal d4     : std_logic_vector(31 downto 0) := (others => '0');
   signal q4     : std_logic_vector(31 downto 0) := (others => '0');
   signal we4    : std_logic;
@@ -59,36 +67,91 @@ architecture Behavioral of memtest is
 
   signal valid : std_logic := '0';
 
-
+  signal addr2readen : std_logic := '0';
+  signal addr2error  : std_logic := '0';
+  signal locked : std_logic_vector(5 downto 0) := (others =>  '0');
+  signal dcmreset : std_logic := '1';
+  
   component memory
-    port ( CLK     : in    std_logic;
-           RESET   : in    std_logic;           DQEXT   : inout std_logic_vector(31 downto 0);
-           WEEXT   : out   std_logic;
-           ADDREXT : out   std_logic_vector(16 downto 0);
-           ADDR1   : in    std_logic_vector(16 downto 0);
-           ADDR2   : in    std_logic_vector(16 downto 0);
-           ADDR3   : in    std_logic_vector(16 downto 0);
-           ADDR4   : in    std_logic_vector(16 downto 0);
-           D1      : in    std_logic_vector(31 downto 0);
-           D2      : in    std_logic_vector(31 downto 0);
-           D3      : in    std_logic_vector(31 downto 0);
-           D4      : in    std_logic_vector(31 downto 0);
-           Q1      : out   std_logic_vector(31 downto 0);
-           Q2      : out   std_logic_vector(31 downto 0);
-           Q3      : out   std_logic_vector(31 downto 0);
-           Q4      : out   std_logic_vector(31 downto 0);
-           WE1     : in    std_logic;
-           WE2     : in    std_logic;
-           WE3     : in    std_logic;
-           WE4     : in    std_logic;
-           CLKEN1  : out   std_logic;
-           CLKEN2  : out   std_logic;
-           CLKEN3  : out   std_logic;
-           CLKEN4  : out   std_logic);
+    port ( CLK     : in  std_logic;
+           RESET   : in  std_logic; DQEXT : inout std_logic_vector(31 downto 0);
+           WEEXT   : out std_logic;
+           ADDREXT : out std_logic_vector(16 downto 0);
+           ADDR1   : in  std_logic_vector(16 downto 0);
+           ADDR2   : in  std_logic_vector(16 downto 0);
+           ADDR3   : in  std_logic_vector(16 downto 0);
+           ADDR4   : in  std_logic_vector(16 downto 0);
+           D1      : in  std_logic_vector(31 downto 0);
+           D2      : in  std_logic_vector(31 downto 0);
+           D3      : in  std_logic_vector(31 downto 0);
+           D4      : in  std_logic_vector(31 downto 0);
+           Q1      : out std_logic_vector(31 downto 0);
+           Q2      : out std_logic_vector(31 downto 0);
+           Q3      : out std_logic_vector(31 downto 0);
+           Q4      : out std_logic_vector(31 downto 0);
+           WE1     : in  std_logic;
+           WE2     : in  std_logic;
+           WE3     : in  std_logic;
+           WE4     : in  std_logic;
+           CLKEN1  : out std_logic;
+           CLKEN2  : out std_logic;
+           CLKEN3  : out std_logic;
+           CLKEN4  : out std_logic);
   end component;
 
 
 begin  -- Behavioral
+
+  clk_dcm : DCM
+    generic map (
+      CLKDV_DIVIDE   => 2.0,
+      CLKFX_MULTIPLY => 5,
+      CLKFX_DIVIDE   => 2)
+    port map (
+      CLK0           => clkbint,        -- 50 MHz dummy feedback loop
+      CLKFB          => clkb,           -- 
+      CLKIN          => CLKIN,
+      CLKFX          => clkfint,        -- 125 Mhz generated clock
+      RST            => RESET,
+      LOCKED => locked(0)
+      );
+
+  dcmreset <= not locked(4);
+  
+  clkhi_dcm : DCM
+    port map (
+      CLK0   => clkint,                 -- 0 degree DCM CLK ouptput
+      CLKFB  => clk,                    -- DCM clock feedback
+      CLK180 => clk180int,
+      CLK90  => clk90int,
+      CLK270 => clk270int,
+      CLKIN  => clkf,
+      RST    => dcmreset
+      );
+
+  clkf_bufg : BUFG port map (
+    I => clkfint,
+    O => clkf);
+
+  clkb_bufg : BUFG port map (
+    I => clkbint,
+    O => clkb);
+
+  clk_bufg : BUFG port map (
+    I => clkint,
+    O => clk);
+
+  clk90_bufg : BUFG port map (
+    I => clk90int,
+    O => clk90 );
+
+  clk180_bufg : BUFG port map (
+    I => clk180int,
+    O => clk180 );
+
+  clk270_bufg : BUFG port map (
+    I => clk270int,
+    O => clk270 );
 
 
   memory_inst : memory
@@ -119,6 +182,10 @@ begin  -- Behavioral
       CLKEN3  => clken3,
       CLKEN4  => clken4);
 
+
+  MCLK <= clk270;
+
+
   PHYRESET <= '0';
 
   WE1 <= '1';
@@ -126,42 +193,46 @@ begin  -- Behavioral
   WE3 <= '0';
   WE4 <= '0';
 
-  MCLK <= clk;
-  clk  <= CLKIN;
-
-  d1    <=  addr1(15 downto 0) & addr1(15 downto 0); 
-  expected <= addr2(15 downto 0) & addr2(15 downto 0); 
-
+  dcmshift:  process (clkf)
+    begin
+      if rising_edge(clkf) then
+        locked(5 downto 1) <= locked(4 downto 0); 
+      end if;
+    end process dcmshift;
+    
   main : process(clk)
   begin
     if rising_edge(clk) then
 
       if clken1 = '1' then
-        addr1 <= addr1 + 1;
-
-
-
+        addr1(15 downto 0) <= addr1(15 downto 0) + 1;
+        d1                 <= addr1(15 downto 0) & addr1(15 downto 0);
       end if;
 
       if clken2 = '1' then
-        addr2    <= addr2 + 1;
-        expected2 <= expected;
-        expected3 <= expected2; 
-      end if;
-
-
-      if clken2 = '1' then
-        if expected3 = q2 then
-          valid <= '1';
-        else
-          valid <= '0';
+        addr2(15 downto 0) <= addr2(15 downto 0) + 1;
+        if addr2(15 downto 0) = X"FFFF" then
+          addr2readen      <= '1';
         end if;
 
+
+      end if;
+
+      if addr2readen = '1' then
+        if clken2 = '1' then
+          if d2 /= (addr2(15 downto 0) - 1) & (addr2(15 downto 0) - 1) then
+            addr2error <= '1';
+          else
+            addr2error <= '0';
+          end if;
+        else
+          addr2error   <= '0';
+        end if;
       end if;
     end if;
-
-
   end process main;
+
+
   LEDPOWER <= valid;
   LED100   <= '0';
   LED1000  <= '0';
